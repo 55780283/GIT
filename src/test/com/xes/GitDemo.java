52511b0e792b6f41e2153ba6f93b2f838efc4a68
liumingwei <55780283@qq.com>
2020-03-15 13:32:39 +0800

    添加注释

--- src/test/com/xes/GitDemo.java
+++ src/test/com/xes/GitDemo.java
@@ -1,7 +1,13 @@
 package com.xes;
 
 public class GitDemo {
+    /**
+     * 添加注释
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         System.out.println("ceshi  ");
     }
+
 }
